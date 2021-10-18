0757c8ad6ccd01d25dad0c765a7728d37bfddf17
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2021-10-18 17:07:21 +0600

    now

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCbzpo9sDn7YZVAk_8KDZJwEFjQV1ZuhNg",
+  authDomain: "healthclinic-web.firebaseapp.com",
+  projectId: "healthclinic-web",
+  storageBucket: "healthclinic-web.appspot.com",
+  messagingSenderId: "1003140782688",
+  appId: "1:1003140782688:web:bfdc99a0ef05e6904b4105",
+  measurementId: "G-8KZJBVVYRL"
+};
+
+export default firebaseConfig;
