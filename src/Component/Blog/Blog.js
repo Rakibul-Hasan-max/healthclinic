441fc157e5f6b91a0d4975bd84167fd7d022aad9
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2021-10-19 11:12:32 +0600

    blogs

--- src/Component/Blog/Blog.js
+++ src/Component/Blog/Blog.js
@@ -1,9 +1,125 @@
 import React from 'react';
+import './Blog.css';
 
 const Blog = () => {
     return (
         <div>
-            <h1>our blog</h1>
+            <div className="headed">
+                <h2>From our blog</h2>
+                <p>A clinic is a health facility that is primarily focused on the care of outpatients.</p>
+            </div>
+            <div className="container blogs">
+                <div class="row row-cols-1 row-cols-md-3 g-4">
+                    <div class="col">
+                        <div class="card h-100">
+                        <img src="https://images.unsplash.com/photo-1518152006812-edab29b069ac?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fG1lZGljYWx8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="..."/>
+                        <div class="card-body">
+                            <h5 class="card-title">When the musics over turn of the light says Jim Morrison</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                        <div class="card-footer">
+                            <small class="text-muted">September 30, 2021</small>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card h-100">
+                        <img src="https://images.unsplash.com/photo-1504439468489-c8920d796a29?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fG1lZGljYWx8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="..."/>
+                        <div class="card-body">
+                            <h5 class="card-title">A place where people can receive medical treatment usually for minor ailments.</h5>
+                            <p class="card-text">Filler text is text that shares some characteristics of a real written text, but is random or otherwise generated.It may be used to display a sample of fonts.</p>
+                        </div>
+                        <div class="card-footer">
+                            <small class="text-muted">June 25, 2021</small>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card h-100">
+                        <img src="https://images.unsplash.com/photo-1511174511562-5f7f18b874f8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjR8fG1lZGljYWx8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="..."/>
+                        <div class="card-body">
+                            <h5 class="card-title">A group meeting for teaching a certain skill and working on individual problems</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                        </div>
+                        <div class="card-footer">
+                            <small class="text-muted">May 5, 2021</small>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card h-100">
+                        <img src="https://images.unsplash.com/photo-1559757148-5c350d0d3c56?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fG1lZGljYWx8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="..."/>
+                        <div class="card-body">
+                            <h5 class="card-title">When the musics over turn of the light says Jim Morrison</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                        <div class="card-footer">
+                            <small class="text-muted">September 30, 2021</small>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card h-100">
+                        <img src="https://images.unsplash.com/photo-1527613426441-4da17471b66d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzB8fG1lZGljYWx8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="..."/>
+                        <div class="card-body">
+                            <h5 class="card-title">When the musics over turn of the light says Jim Morrison</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                        <div class="card-footer">
+                            <small class="text-muted">September 30, 2021</small>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card h-100">
+                        <img src="https://images.unsplash.com/photo-1618015359994-a67bd07e48b5?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzJ8fG1lZGljYWx8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="..."/>
+                        <div class="card-body">
+                            <h5 class="card-title">When the musics over turn of the light says Jim Morrison</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                        <div class="card-footer">
+                            <small class="text-muted">September 30, 2021</small>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card h-100">
+                        <img src="https://images.unsplash.com/photo-1581093448798-fa7eab922c96?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzh8fG1lZGljYWx8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="..."/>
+                        <div class="card-body">
+                            <h5 class="card-title">When the musics over turn of the light says Jim Morrison</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                        <div class="card-footer">
+                            <small class="text-muted">September 30, 2021</small>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card h-100">
+                        <img src="https://images.unsplash.com/photo-1579684453423-f84349ef60b0?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzd8fG1lZGljYWx8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="..."/>
+                        <div class="card-body">
+                            <h5 class="card-title">When the musics over turn of the light says Jim Morrison</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                        <div class="card-footer">
+                            <small class="text-muted">September 30, 2021</small>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card h-100">
+                        <img src="https://images.unsplash.com/photo-1618498082410-b4aa22193b38?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTN8fG1lZGljYWx8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="..."/>
+                        <div class="card-body">
+                            <h5 class="card-title">When the musics over turn of the light says Jim Morrison</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                        <div class="card-footer">
+                            <small class="text-muted">September 30, 2021</small>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
