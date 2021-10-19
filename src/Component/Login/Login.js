83f0cd2dbf61f0e20e69f74287f2fa347ced1e68
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2021-10-19 20:50:48 +0600

    ggggg

--- src/Component/Login/Login.js
+++ src/Component/Login/Login.js
@@ -9,29 +9,44 @@ const Login = () => {
     // const {signInUsingGoogle} = useAuth();
     const {signInUsingGoogle} = useFirebase();
 
+    const handleEmailChange = e =>{
+    console.log(e.target.value);
+    }
+
+    const handlePasswordChange = e =>{
+    console.log(e.target.value);
+    }
+
+    const handleRegistration = e => {
+	console.log('regi done');
+    e.preventDefault();
+    }
+
     return (
         <div>
             <div className=" container body">
                 <div className="row align-items-center" style={{ height: "80vh" }}>
                     <div className="col-md-6 shadow p-5">
-                        <div>
+                        <div onSubmit={handleRegistration}>
+                            <div>
                             <h2>Please Login</h2>
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="">User Email</label>
-                            <input type="text" className="form-control" />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="">Password</label>
-                            <input type="password" className="form-control" />
-                        </div>
-                        <div className="form-group">
-                            <input class="form-check-input" type="checkbox" value="" id="flexCheckIndeterminate"></input>
-                            <label htmlFor="" className="text-danger ms-2">Forgot your password?</label>
-                            <Link className="ms-5" to="/register">Register now</Link>
-                        </div>
-                        <div className="from-group mt-3">
-                            <button className="btn btn-success" type="submit">Login</button>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="">User Email</label>
+                                <input onBlur={handleEmailChange} type="text" className="form-control" />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="">Password</label>
+                                <input onBlur={handlePasswordChange} type="password" className="form-control" />
+                            </div>
+                            <div className="form-group">
+                                <input class="form-check-input" type="checkbox" value="" id="flexCheckIndeterminate"></input>
+                                <label htmlFor="" className="text-danger ms-2">Forgot your password?</label>
+                                <Link className="ms-5" to="/register">Register now</Link>
+                            </div>
+                            <div className="from-group mt-3">
+                                <button onClick={handleRegistration} className="btn btn-success" type="submit">Login</button>
+                            </div>
                         </div>
                         <div className="from-group mt-5">
                             <button onClick={signInUsingGoogle} className="btn btn-primary" >Google Sign in</button>
